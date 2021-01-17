ab69e7b2dfe758b7b163724176f7a43c20ea622c
Claire Melbourne <claire.melbourne@gmail.com>
2021-01-17 12:54:17 -0600

    Set up react filepaths and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/public/src');
+const DIST_DIR = path.join(__dirname, '/public/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: SRC_DIR,
+        use: {
+          loader: 'bable-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
